90e62b0be905d4a0549f87b84ccd79d629e3d961
KPaezAstudillo <karinapaezastudillo@gmail.com>
2023-02-16 01:11:06 -0300

    created components

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <Main />
+    <AboutMe />
+    <Projects />
+    <Footer />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
